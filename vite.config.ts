284fa75f9026eb07c4eda7380ff7845c4ee0fd5c
Juan Andres Gualotuna <jgualotu@espol.edu.ec>
2025-12-17 05:22:21 +0000

    "Se agregó los archivos restantes en public"

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,59 @@ export default defineConfig({
       registerType: 'autoUpdate',
       devOptions: {
         enabled: true
-      }
+      },
+       includeAssets: ['favicon.ico', 'apple-touch-icon.png'],
+   manifest: {
+      id: '/Dashboard_dawm/',
+      name: 'Dashboard del Clima - Proyecto 04',
+      short_name: 'Dashboard del Clima',
+      description: 'Proyecto 04 - dashboard del clima desarrollado con React y MUI',
+      theme_color: '#D3D1D1',
+      icons: [
+         {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+         },
+         {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+         },
+         {
+            src: "pwa-maskable-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "maskable"
+         },
+         {
+            src: "pwa-maskable-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable"
+         },
+      ]
+   },
+   workbox: {
+      runtimeCaching: [
+         {
+            // Intercepta todas las peticiones a esta API (ajusta según necesidad)
+            urlPattern: /^https:\/\/api\.open-meteo\.com\/.*$/,
+            handler: 'NetworkFirst',
+            options: {
+               cacheName: 'open-meteo-cache',
+               expiration: {
+                  maxEntries: 10,
+                  maxAgeSeconds: 60 * 60 * 24, // 1 día
+               },
+               cacheableResponse: {
+                  statuses: [0, 200],
+               }
+            }
+         }
+      ]
+   }
+
     })
   ],
 })
